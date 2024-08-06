74083b58403193aae4efcd0456cb727c7e17b057
Oluwole Akani <oakani@gmail.com>
2024-08-06 12:44:39 +0000

    urls upate, bug fix needed

--- tasks/views.py
+++ tasks/views.py
@@ -4,3 +4,9 @@ from .models import Task
 # Create your views here.
 def show_task(request, task_id):
     retrieved_task = get_object_or_404(Task, id=task_id)
+
+    context = {
+        "task": retrieved_task
+    }
+
+    return render(request, 'tasks/view_task.html', context)
