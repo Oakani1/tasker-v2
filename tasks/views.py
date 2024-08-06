3c3685cf3cbbeeffedeece7336c7f6b8e909fe4b
Oluwole Akani <oakani@gmail.com>
2024-08-06 12:56:21 +0000

    viewing a task

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task
+
 
 # Create your views here.
 def show_task(request, task_id):
     retrieved_task = get_object_or_404(Task, id=task_id)
 
     context = {
-        "task": retrieved_task
+        "task": retrieved_task,
     }
 
     return render(request, 'tasks/view_task.html', context)
