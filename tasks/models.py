7eb29605e7828c2ae53d1e71123ec6106b88dc3b
Oluwole Akani <oakani@gmail.com>
2024-08-06 12:01:34 +0000

    added model and view task HTML.. need to test

--- tasks/models.py
+++ tasks/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Pending"), (1, "In progress"), (2, "Complete"), (3, "Late"))
 
-class task(models.Model):
+class Task(models.Model):
     task_name = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField(blank=True)
-    user_id = models.ForeignKey(
+    user_name = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="task"
     )
     start_time = models.DateTimeField(null=False, blank=False)
     progress = models.IntegerField(null=False, blank=False)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__ (self):
+        return f'{self.task_name}'
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+def show_task(request, task_id):
+    retrieved_task = get_object_or_404(Task, id=task_id)
