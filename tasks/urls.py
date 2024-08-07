a39f856faf95426260f1ad4442504a91a5d8286f
Oluwole Akani <oakani@gmail.com>
2024-08-07 14:59:15 +0000

    edit task

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("view/<task_id>", views.show_task, name="view"),
     path("homepage/", views.show_homepage, name="home"),
     path("create/", views.create_task, name="create"),
+    path("edit/<task_id>", views.edit_task, name="edit"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -28,7 +28,26 @@ def create_task(request):
     else:
         #handle a GET Request
         form = TaskForm()
-        context = {
-            "form": form,
-        }
+        context = {"form": form, }
         return render(request, 'tasks/create_task.html', context)
+
+def edit_task(request, task_id):
+    retrieved_task = get_object_or_404(Task, id=task_id)
+
+    if not request.user == retrieved_task.user_name:
+        messages.error(request, "you can not edit a task that you did not create")
+        return redirect("home")
+
+    if request.method =="POST":
+        pass
+        """form = TaskForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            messages.success(request, "Task Saved!")
+            return redirect("home")"""
+      
+    else:   
+        form = TaskForm(instance=retrieved_task)
+        context = {"form": form, }
+        return render(request, 'tasks/edit_task.html', context)
+        
