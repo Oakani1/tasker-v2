c16eedadb4ef1d0ca1fc9fcdf3cf6dbfc8705b90
Oluwole Akani <oakani@gmail.com>
2024-08-07 16:43:01 +0000

    create, edit , delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("view/<task_id>", views.show_task, name="view"),
-    path("homepage/", views.show_homepage, name="home"),
+    path("", views.show_homepage, name="home"),
     path("create/", views.create_task, name="create"),
     path("edit/<task_id>", views.edit_task, name="edit"),
+    path("delete/<task_id>", views.delete_task, name="delete"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -22,7 +22,7 @@ def create_task(request):
         form = TaskForm(request.POST)
         if form.is_valid() :
             form.save()
-            messages.success(request, "Task Saved!")
+            messages.success(request, "Your task is Saved!")
             return redirect("home")
       
     else:
@@ -35,19 +35,35 @@ def edit_task(request, task_id):
     retrieved_task = get_object_or_404(Task, id=task_id)
 
     if not request.user == retrieved_task.user_name:
-        messages.error(request, "you can not edit a task that you did not create")
+        messages.error(request, "You can not edit a task that you did not create")
         return redirect("home")
 
     if request.method =="POST":
-        pass
-        """form = TaskForm(request.POST)
+        
+        form = TaskForm(request.POST, instance=retrieved_task)
         if form.is_valid() :
             form.save()
-            messages.success(request, "Task Saved!")
-            return redirect("home")"""
+            messages.success(request, "Your task is updated!")
+            return redirect("home")
       
     else:   
         form = TaskForm(instance=retrieved_task)
         context = {"form": form, }
         return render(request, 'tasks/edit_task.html', context)
+
+def delete_task(request, task_id):
+    retrieved_task = get_object_or_404(Task, id=task_id)
+
+    if not request.user == retrieved_task.user_name:
+        messages.error(request, "You can not delete a task that you did not create")
+        return redirect("home")
+
+    if request.method =="POST":
+        retrieved_task.delete()
+        messages.success(request, "Your task was deleted")
+        return redirect("home")
+      
+    else:   
+        context = {"task": retrieved_task, }
+        return render(request, 'tasks/delete_task.html', context)    
         
